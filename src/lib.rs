992dc5e04807719e27a9510f675ea3c69df870be
mikev <mvierling@gmail.com>
2021-12-06 21:18:00 -0800

    remove dead code

--- src/lib.rs
+++ src/lib.rs
@@ -25,31 +25,33 @@ pub fn test_mac() -> () {
     mac2.verify(&tag_bytes).unwrap()
 }
 
-pub fn create_mac(fcnt: u16) -> &'static [u16] {
+pub fn create_test_mac(fcnt: u16) -> &'static [u16] {
     let mut mac = Cmac::<Aes128>::new_from_slice(b"very secret key.").unwrap();
     let bytes = fcnt.to_be_bytes();
     mac.update(&bytes);
     let result = mac.finalize();
     let tag_bytes = result.into_bytes();
-    println!("create_mac: tag_bytes.len()={}", tag_bytes.len());
+    println!("create_test_mac: tag_bytes.len()={}", tag_bytes.len());
     let array_u8: [u8; 16] = tag_bytes.as_slice().try_into().expect("Wrong length");
     unsafe {
         let (_prefix, shorts, _suffix) = array_u8.align_to::<u16>();
         let array_u16 = &shorts[0..7];
         let boxed_data: Box<[u16]> = array_u16.iter().cloned().collect();
-        println!("create_mac: fcnt={} mac={:?}", fcnt, boxed_data);
+        println!("create_test_mac: fcnt={} mac={:?}", fcnt, boxed_data);
         Box::leak(boxed_data)
     }
 }
 
-type SuperMICState = array2d::Array2D<u16>;
-type SuperMIC = u16;
+type CuMICState = array2d::Array2D<u16>;
+type CuMIC = u16;
 
-pub fn zero_state() -> SuperMICState {
-    Array2D::fill_with(0u16, 8, 8)
+pub fn zero_state() -> CuMICState {
+    let array = Array2D::fill_with(0u16, 8, 8);
+	assert_eq!(array.as_rows(), vec![vec![42, 42, 42], vec![42, 42, 42]]);
+	array
 }
 
-pub fn mutate_state(fcnt: u16, mic: &[u16], state: SuperMICState) -> (SuperMICState, u16) {
+pub fn mutate_state(fcnt: u16, mic: &[u16], state: CuMICState) -> (CuMICState, u16) {
     let row: usize = (fcnt % 8) as usize;
     let mut array = state;
     for col in 0..7 {
@@ -65,7 +67,7 @@ pub fn mutate_state(fcnt: u16, mic: &[u16], state: SuperMICState) -> (SuperMICSt
     (array, tag)
 }
 
-pub fn generate_tag(fcnt: u16, state: SuperMICState) -> SuperMIC {
+pub fn generate_tag(fcnt: u16, state: CuMICState) -> CuMIC {
     let mut tag: u16 = 0;
     let col: usize = (fcnt % 8) as usize;
     for element in state.column_iter(col) {
@@ -75,26 +77,36 @@ pub fn generate_tag(fcnt: u16, state: SuperMICState) -> SuperMIC {
     tag
 }
 
+fn mic_loop() {
+
+	let mut state: CuMICState = zero_state();
+	for i in 0..100 {
+		let mac0 = create_test_mac(i);
+		let (state1, _mic0) = mutate_state(i, mac0, state);
+		state = state1;
+	}
+}
+
 pub fn test_mic() {
     println!("test_mic");
     let state0 = zero_state();
     let mut fcnt = 0;
-    let mac0 = create_mac(fcnt);
+    let mac0 = create_test_mac(fcnt);
     let (state1, _mic0) = mutate_state(fcnt, mac0, state0);
     //let _mic0 = generate_tag(fcnt, state1);
     // end-device puts mic0 into 16-bit mic
     // LNS mirrors the process (with the same key) and gets the same mic0
     fcnt = fcnt + 1;
-    let mac1 = create_mac(fcnt);
+    let mac1 = create_test_mac(fcnt);
     let (state2, _mic1) = mutate_state(fcnt, mac1, state1);
     //let _mic1 = generate_tag(fcnt, state2);
     fcnt = fcnt + 1;
-    let mac2 = create_mac(fcnt);
+    let mac2 = create_test_mac(fcnt);
     let (state3, _mic2) = mutate_state(fcnt, mac2, state2);
     //let _mic2 = generate_tag(fcnt, state3);
     fcnt = fcnt + 1;
-    let mac3 = create_mac(fcnt);
+    let mac3 = create_test_mac(fcnt);
     let (_state4, _mic3) = mutate_state(fcnt, mac3, state3);
-    //let _mic3 = generate_tag(fcnt, state4);
-    //fcnt = fcnt + 1;
+
+	mic_loop();
 }
