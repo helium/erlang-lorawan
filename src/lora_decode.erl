34f7bf83c894c381a08b7883db1708c3f57ffc19
mikev <mvierling@gmail.com>
2022-01-25 16:32:33 -0800

    checkpoint

---
+++ src/lora_decode.erl
@@ -0,0 +1,160 @@
+%%%-------------------------------------------------------------------
+%% @doc
+%% == LoRaWAN ==
+%% @end
+%%%-------------------------------------------------------------------
+-module(lora_decode).
+
+-export([
+    %% public functions
+    sample1/0,
+    base64_to_binary/1,
+    payload_mhdr/1,
+    %% internal functions
+    payload_test/0
+]).
+
+-define(Join_Request, 2#000).
+-define(Join_Accept, 2#001).
+-define(Unconfirmed_Uplink, 2#010).
+-define(Unconfirmed_Downlink, 2#011).
+-define(Confirmed_Uplink, 2#100).
+-define(Confirmed_Downlink, 2#101).
+-define(RFU, 2#110).
+-define(Proprietary, 2#111).
+
+-spec base64_to_binary(binary()) -> binary().
+base64_to_binary(Data) ->
+    base64:decode(Data).
+
+-spec payload_mhdr(binary()) -> binary().
+payload_mhdr(PhyPayload) ->
+    <<MHDR:8/integer-unsigned, _/binary>> = PhyPayload,
+    <<MHDR>>.
+
+payload_direction(PhyPayload) ->
+    <<_Ignore:2/integer-unsigned, DirectionBit:1/integer-unsigned, _Ignore2:5/integer, _/binary>> = PhyPayload,
+    case DirectionBit of
+        0 -> <<"up">>;
+        1 -> <<"down">>
+    end.
+
+payload_mic(PhyPayload) ->
+    PayloadSize = byte_size(PhyPayload),
+    Part = {PayloadSize, -4},
+    MIC = binary:part(PhyPayload, Part),
+    MIC.
+
+payload_macpayload(PhyPayload) ->
+    PayloadSize = byte_size(PhyPayload),
+    MacPayloadSize = PayloadSize - 5,
+    Part = {1, MacPayloadSize},
+    MacPayload = binary:part(PhyPayload, Part),
+    MacPayload.
+
+payload_ftype(PhyPayload) ->
+    MHDR = payload_mhdr(PhyPayload),
+    ftype_from_mhdr(MHDR).
+
+payload_fcnt(PhyPayload) ->
+    <<_MHDR:8/integer, _DevAddr:32/integer, _FCtrl:8/integer-unsigned, FCnt:16/little-integer-unsigned, _/binary>> = PhyPayload,
+    FCnt.
+
+payload_devaddr(PhyPayload) ->
+    <<_MHDR:8/integer, DevAddr:32/integer, _/binary>> = PhyPayload,
+    DevAddr.
+
+payload_fctrl(PhyPayload) ->
+    <<_MHDR:8/integer, _DevAddr:32/integer, FCtrl:8/little-integer-unsigned, _/binary>> = PhyPayload,
+    FCtrl.
+
+payload_foptslen(PhyPayload) ->
+    <<_MHDR:8/integer, _DevAddr:32/integer, _Ignore:4/integer-unsigned, Foptslen:4/integer-unsigned, _/binary>> = PhyPayload,
+    Foptslen.
+
+payload_fopts(PhyPayload) ->
+    Len = payload_foptslen(PhyPayload),
+    %% Offset == MHDR + DevAddr + FCtrl + FCnt
+    Offset = 1 + 4 + 1 + 2,
+    Part = {Offset, Len},
+    FOpt = binary:part(PhyPayload, Part),
+    FOpt.
+
+payload_fhdr(PhyPayload) ->
+    FOptsLen = payload_foptslen(PhyPayload),
+    FhdrLen = 7 + FOptsLen,
+    Part = {1, FhdrLen},
+    FHDR = binary:part(PhyPayload, Part),
+    FHDR.
+
+ftype_from_mhdr(MHDR) -> 
+    <<FType:3/integer-unsigned, _RFU:3/integer-unsigned, _Major:2/integer-unsigned>> = MHDR,
+    FType. 
+
+%% ==================================================================
+%% Tests
+%% ==================================================================
+%%-ifdef(EUNIT).
+
+-include_lib("eunit/include/eunit.hrl").
+
+sample0() ->
+    <<"QHcQASaAFAABvRjrSjJcz6vXC2TMw1A=">>.
+sample1() ->
+    <<"YAQAAEiqLgADUwAAcANTAP8ADY5nmA==">>.
+
+bin_to_hex(Binary) ->
+    [[io_lib:format("~2.16.0B",[X]) || <<X:8>> <= Binary ]].
+
+decode_payload(Base64) ->
+    io:format("~nAssuming base64-encoded packet~n"),
+    io:format("~s~n", [Base64]),
+    Bin0 = base64_to_binary(Base64),
+    io:format("Binary packet = ~w~n", [Bin0]),
+
+    io:format("~n( PHYPayload = MHDR[1] | MACPayload[..] | MIC[4] )~n"),
+    MHDR = payload_mhdr(Bin0),
+    %% io:format("Binary ~8.16.0B~n", [MHDR]),
+    io:format("MHDR = ~w~n", [MHDR]),
+    MacPayload = payload_macpayload(Bin0),
+    io:format("MacPayload = ~w~n", [MacPayload]),
+    io:format("MacPayload = ~s~n", [bin_to_hex(MacPayload)]),
+    MIC = payload_mic(Bin0),
+    io:format("MIC = ~s~n", [bin_to_hex(MIC)]),
+
+    io:format("~n( MACPayload = FHDR | FPort | FRMPayload )~n"),
+    FHDR = payload_fhdr(Bin0),
+    io:format("FHDR = ~w~n", [FHDR]),
+    io:format("FHDR = ~s~n", [bin_to_hex(FHDR)]),
+    io:format("FPort = ~w~n", [0]),
+    io:format("FRMPayload = ~w~n", [0]),
+
+    io:format("~n( FHDR = DevAddr[4] | FCtrl[1] | FCnt[2] | FOpts[0..15] )~n"),
+    DevAddr = payload_devaddr(Bin0),
+    io:format("DevAddr = ~8.16.0B~n", [DevAddr]),
+    FCtrl = payload_fctrl(Bin0),
+    io:format("FCtrl = ~w~n", [FCtrl]),
+    FCnt = payload_fcnt(Bin0),
+    io:format("FCnt = ~w~n", [FCnt]),
+    FOptsLen = payload_foptslen(Bin0),
+    io:format("FOptsLen = ~w~n", [FOptsLen]),
+    FOpts = payload_fopts(Bin0),
+    io:format("FOpts = ~w~n", [FOpts]),
+    io:format("FOpts = ~s~n", [bin_to_hex(FOpts)]),
+
+    FType = payload_ftype(Bin0),
+    io:format("~nMessage Type = ~w~n", [FType]),
+    Direction = payload_direction(Bin0),
+    io:format("Direction = ~s~n", [Direction]),
+    FCnt2 = payload_fcnt(Bin0),
+    io:format("FCnt = ~w~n", [FCnt2]),
+    fin.
+
+payload_test() ->
+    Pay0 = sample0(),
+    Pay1 = sample1(),
+    decode_payload(Pay0),
+    decode_payload(Pay1),
+    fin.
+
+%%-endif.
