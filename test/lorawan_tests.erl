f9cbc28d9b254d2c5af064f17d70a5383c9eb0c0
mikev <mvierling@gmail.com>
2022-03-04 11:07:34 -0800

    rename lorawan module to lora_subnet

--- test/lorawan_tests.erl
+++ test/lorawan_tests.erl
@@ -11,67 +11,67 @@
 id_test() ->
     %% CP data
     ?assertEqual(
-        {ok, 16#00002D}, lorawan:parse_netid(<<91, 255, 255, 255>>), "[45] == 2D == 45 type 0"
+        {ok, 16#00002D}, lora_subnet:parse_netid(<<91, 255, 255, 255>>), "[45] == 2D == 45 type 0"
     ),
     ?assertEqual(
-        {ok, 16#20002D}, lorawan:parse_netid(<<173, 255, 255, 255>>), "[45] == 2D == 45 type 1"
+        {ok, 16#20002D}, lora_subnet:parse_netid(<<173, 255, 255, 255>>), "[45] == 2D == 45 type 1"
     ),
     ?assertEqual(
-        {ok, 16#40016D}, lorawan:parse_netid(<<214, 223, 255, 255>>), "[1,109] == 16D == 365 type 2"
+        {ok, 16#40016D}, lora_subnet:parse_netid(<<214, 223, 255, 255>>), "[1,109] == 16D == 365 type 2"
     ),
     ?assertEqual(
         {ok, 16#6005B7},
-        lorawan:parse_netid(<<235, 111, 255, 255>>),
+        lora_subnet:parse_netid(<<235, 111, 255, 255>>),
         "[5,183] == 5B7 == 1463 type 3"
     ),
     ?assertEqual(
         {ok, 16#800B6D},
-        lorawan:parse_netid(<<245, 182, 255, 255>>),
+        lora_subnet:parse_netid(<<245, 182, 255, 255>>),
         "[11, 109] == B6D == 2925 type 4"
     ),
     ?assertEqual(
         {ok, 16#A016DB},
-        lorawan:parse_netid(<<250, 219, 127, 255>>),
+        lora_subnet:parse_netid(<<250, 219, 127, 255>>),
         "[22,219] == 16DB == 5851 type 5"
     ),
     ?assertEqual(
         {ok, 16#C05B6D},
-        lorawan:parse_netid(<<253, 109, 183, 255>>),
+        lora_subnet:parse_netid(<<253, 109, 183, 255>>),
         "[91, 109] == 5B6D == 23405 type 6"
     ),
     ?assertEqual(
         {ok, 16#E16DB6},
-        lorawan:parse_netid(<<254, 182, 219, 127>>),
+        lora_subnet:parse_netid(<<254, 182, 219, 127>>),
         "[1,109,182] == 16DB6 == 93622 type 7"
     ),
     ?assertEqual(
         {error, invalid_netid_type},
-        lorawan:parse_netid(<<255, 255, 255, 255>>),
+        lora_subnet:parse_netid(<<255, 255, 255, 255>>),
         "Invalid DevAddr"
     ),
 
     % Actility spreadsheet examples
-    ?assertEqual({ok, 0}, lorawan:parse_netid(<<0:1, 0:1, 0:1, 0:1, 0:1, 0:1, 0:1, 0:25>>)),
-    ?assertEqual({ok, 1}, lorawan:parse_netid(<<0:1, 0:1, 0:1, 0:1, 0:1, 0:1, 1:1, 0:25>>)),
-    ?assertEqual({ok, 2}, lorawan:parse_netid(<<0:1, 0:1, 0:1, 0:1, 0:1, 1:1, 0:1, 0:25>>)),
+    ?assertEqual({ok, 0}, lora_subnet:parse_netid(<<0:1, 0:1, 0:1, 0:1, 0:1, 0:1, 0:1, 0:25>>)),
+    ?assertEqual({ok, 1}, lora_subnet:parse_netid(<<0:1, 0:1, 0:1, 0:1, 0:1, 0:1, 1:1, 0:25>>)),
+    ?assertEqual({ok, 2}, lora_subnet:parse_netid(<<0:1, 0:1, 0:1, 0:1, 0:1, 1:1, 0:1, 0:25>>)),
 
     %% Mis-parsed as netid 4 of type 3
     ?assertEqual(
-        {ok, 16#600004}, lorawan:parse_netid(<<224, 9, 171, 205>>), "hex_to_binary(<<'E009ABCD'>>)"
+        {ok, 16#600004}, lora_subnet:parse_netid(<<224, 9, 171, 205>>), "hex_to_binary(<<'E009ABCD'>>)"
     ),
     %% Valid DevAddr, NetID not assigned
     ?assertEqual(
         {ok, 16#20002D},
-        lorawan:parse_netid(<<173, 255, 255, 255>>),
+        lora_subnet:parse_netid(<<173, 255, 255, 255>>),
         "hex_to_binary(<<'ADFFFFFF'>>)"
     ),
     %% Less than 32 bit number
-    ?assertEqual({ok, 0}, lorawan:parse_netid(46377)),
+    ?assertEqual({ok, 0}, lora_subnet:parse_netid(46377)),
 
     % Louis test data
-    ?assertEqual({ok, 16#600002}, lorawan:parse_netid(<<224, 4, 0, 1>>)),
-    ?assertEqual({ok, 16#600002}, lorawan:parse_netid(<<224, 5, 39, 132>>)),
-    ?assertEqual({ok, 16#000002}, lorawan:parse_netid(<<4, 16, 190, 163>>)),
+    ?assertEqual({ok, 16#600002}, lora_subnet:parse_netid(<<224, 4, 0, 1>>)),
+    ?assertEqual({ok, 16#600002}, lora_subnet:parse_netid(<<224, 5, 39, 132>>)),
+    ?assertEqual({ok, 16#000002}, lora_subnet:parse_netid(<<4, 16, 190, 163>>)),
     ok.
 
 create_netid(NetClass, ID) ->
@@ -98,13 +98,13 @@ insert_rand(Item, List) ->
     NewList.
 
 exercise_subnet(DevAddr, NetIDList) ->
-    SubnetAddr = lorawan:subnet_from_devaddr(DevAddr, NetIDList),
-    DevAddr2 = lorawan:devaddr_from_subnet(SubnetAddr, NetIDList),
+    SubnetAddr = lora_subnet:subnet_from_devaddr(DevAddr, NetIDList),
+    DevAddr2 = lora_subnet:devaddr_from_subnet(SubnetAddr, NetIDList),
     ?assertEqual(DevAddr, DevAddr2),
     ok.
 
 exercise_subnet(DevAddr) ->
-    {ok, NetID} = lorawan:parse_netid(DevAddr),
+    {ok, NetID} = lora_subnet:parse_netid(DevAddr),
     exercise_subnet(DevAddr, insert_item(NetID, mock_netid_list(), 0)),
     exercise_subnet(DevAddr, insert_item(NetID, mock_netid_list(), 1)),
     exercise_subnet(DevAddr, insert_item(NetID, mock_netid_list(), 2)),
@@ -112,19 +112,19 @@ exercise_subnet(DevAddr) ->
     ok.
 
 random_subnet(DevAddr) ->
-    {ok, NetID} = lorawan:parse_netid(DevAddr),
+    {ok, NetID} = lora_subnet:parse_netid(DevAddr),
     [exercise_subnet(DevAddr, insert_rand(NetID, mock_random_netids())) || _ <- lists:seq(1, 400)],
     ok.
 
 exercise_devaddr(NetID, Addr, _IDLen, AddrLen) ->
-    DevAddr = lorawan:devaddr(NetID, Addr),
-    NetIDType = lorawan:netid_type(DevAddr),
+    DevAddr = lora_subnet:devaddr(NetID, Addr),
+    NetIDType = lora_subnet:netid_type(DevAddr),
     ?assert(NetIDType =< 7),
-    {ok, NetID0} = lorawan:parse_netid(DevAddr),
+    {ok, NetID0} = lora_subnet:parse_netid(DevAddr),
     ?assertEqual(NetID, NetID0),
-    AddrBitLen = lorawan:addr_bit_len(DevAddr),
+    AddrBitLen = lora_subnet:addr_bit_len(DevAddr),
     ?assertEqual(AddrLen, AddrBitLen),
-    NwkAddr = lorawan:nwk_addr(DevAddr),
+    NwkAddr = lora_subnet:nwk_addr(DevAddr),
     ?assertEqual(Addr, NwkAddr),
     exercise_subnet(DevAddr),
     random_subnet(DevAddr),
@@ -133,9 +133,9 @@ exercise_devaddr(NetID, Addr, _IDLen, AddrLen) ->
 exercise_netid(NetClass, ID, IDLen, AddrLen) ->
     NetIDBin = <<0:8/integer-unsigned, NetClass:3/integer-unsigned, ID:21/integer-unsigned>>,
     <<NetID:32/integer-unsigned>> = NetIDBin,
-    NetAddrLen = lorawan:addr_len(NetClass),
+    NetAddrLen = lora_subnet:addr_len(NetClass),
     ?assert(NetAddrLen == AddrLen),
-    MaxNetSize = lorawan:netid_size(NetID),
+    MaxNetSize = lora_subnet:netid_size(NetID),
     exercise_devaddr(NetID, 0, IDLen, AddrLen),
     exercise_devaddr(NetID, 1, IDLen, AddrLen),
     exercise_devaddr(NetID, 8, IDLen, AddrLen),
@@ -182,83 +182,83 @@ netid_test() ->
     %% DevAddr with class 3 NetID
     DevAddr02 = 16#E05A0008,
 
-    NetWidth0 = lorawan:addr_len(lorawan:netid_class(NetID00)),
+    NetWidth0 = lora_subnet:addr_len(lora_subnet:netid_class(NetID00)),
     ?assertEqual(7, NetWidth0),
-    NetWidth1 = lorawan:addr_len(lorawan:netid_class(NetID01)),
+    NetWidth1 = lora_subnet:addr_len(lora_subnet:netid_class(NetID01)),
     ?assertEqual(10, NetWidth1),
-    NetWidth2 = lorawan:addr_len(lorawan:netid_class(NetID02)),
+    NetWidth2 = lora_subnet:addr_len(lora_subnet:netid_class(NetID02)),
     ?assertEqual(17, NetWidth2),
-    NetSize0 = lorawan:netid_size(NetID00),
+    NetSize0 = lora_subnet:netid_size(NetID00),
     ?assertEqual(128, NetSize0),
-    NetSize1 = lorawan:netid_size(NetID01),
+    NetSize1 = lora_subnet:netid_size(NetID01),
     ?assertEqual(1024, NetSize1),
-    NetSize2 = lorawan:netid_size(NetID02),
+    NetSize2 = lora_subnet:netid_size(NetID02),
     ?assertEqual(131072, NetSize2),
 
     NetIDList = [NetID00, NetID01, NetID02],
-    LocalTrue = lorawan:is_local_netid(NetID01, NetIDList),
-    LocalFalse = lorawan:is_local_netid(NetIDExt, NetIDList),
-    LegacyLocal = lorawan:is_local_netid(LegacyNetID, NetIDList),
+    LocalTrue = lora_subnet:is_local_netid(NetID01, NetIDList),
+    LocalFalse = lora_subnet:is_local_netid(NetIDExt, NetIDList),
+    LegacyLocal = lora_subnet:is_local_netid(LegacyNetID, NetIDList),
     ?assertEqual(true, LocalTrue),
     ?assertEqual(false, LocalFalse),
     ?assertEqual(true, LegacyLocal),
 
-    DevAddrLegacy = lorawan:devaddr(LegacyNetID, 0),
+    DevAddrLegacy = lora_subnet:devaddr(LegacyNetID, 0),
     ?assertEqual(DevAddr00, DevAddrLegacy),
-    DevAddr1 = lorawan:devaddr(NetID01, 16),
+    DevAddr1 = lora_subnet:devaddr(NetID01, 16),
     ?assertEqual(DevAddr01, DevAddr1),
-    DevAddr2 = lorawan:devaddr(NetID02, 8),
+    DevAddr2 = lora_subnet:devaddr(NetID02, 8),
     ?assertEqual(DevAddr02, DevAddr2),
 
-    NetIDType00 = lorawan:netid_type(DevAddr00),
+    NetIDType00 = lora_subnet:netid_type(DevAddr00),
     ?assertEqual(1, NetIDType00),
-    NetIDType01 = lorawan:netid_type(DevAddr01),
+    NetIDType01 = lora_subnet:netid_type(DevAddr01),
     ?assertEqual(6, NetIDType01),
-    NetIDType02 = lorawan:netid_type(DevAddr02),
+    NetIDType02 = lora_subnet:netid_type(DevAddr02),
     ?assertEqual(3, NetIDType02),
 
-    NetIDType0 = lorawan:netid_type(DevAddrLegacy),
+    NetIDType0 = lora_subnet:netid_type(DevAddrLegacy),
     ?assertEqual(1, NetIDType0),
-    NetIDType1 = lorawan:netid_type(DevAddr1),
+    NetIDType1 = lora_subnet:netid_type(DevAddr1),
     ?assertEqual(6, NetIDType1),
-    NetIDType2 = lorawan:netid_type(DevAddr2),
+    NetIDType2 = lora_subnet:netid_type(DevAddr2),
     ?assertEqual(3, NetIDType2),
 
-    {ok, NetID_0} = lorawan:parse_netid(DevAddr00),
+    {ok, NetID_0} = lora_subnet:parse_netid(DevAddr00),
     ?assertEqual(NetID_0, LegacyNetID),
-    {ok, NetID_1} = lorawan:parse_netid(16#FC00D410),
+    {ok, NetID_1} = lora_subnet:parse_netid(16#FC00D410),
     ?assertEqual(NetID_1, 16#C00035),
-    {ok, NetID_1} = lorawan:parse_netid(DevAddr01),
+    {ok, NetID_1} = lora_subnet:parse_netid(DevAddr01),
     ?assertEqual(NetID_1, NetID01),
-    {ok, NetID_2} = lorawan:parse_netid(DevAddr02),
+    {ok, NetID_2} = lora_subnet:parse_netid(DevAddr02),
     ?assertEqual(NetID_2, NetID02),
 
-    {ok, NetID0} = lorawan:parse_netid(DevAddrLegacy),
+    {ok, NetID0} = lora_subnet:parse_netid(DevAddrLegacy),
     ?assertEqual(NetID0, LegacyNetID),
-    {ok, NetID1} = lorawan:parse_netid(DevAddr1),
+    {ok, NetID1} = lora_subnet:parse_netid(DevAddr1),
     ?assertEqual(NetID1, NetID01),
-    {ok, NetID2} = lorawan:parse_netid(DevAddr2),
+    {ok, NetID2} = lora_subnet:parse_netid(DevAddr2),
     ?assertEqual(NetID2, NetID02),
 
-    Width_0 = lorawan:addr_bit_len(DevAddr00),
+    Width_0 = lora_subnet:addr_bit_len(DevAddr00),
     ?assertEqual(24, Width_0),
-    Width_1 = lorawan:addr_bit_len(DevAddr01),
+    Width_1 = lora_subnet:addr_bit_len(DevAddr01),
     ?assertEqual(10, Width_1),
-    Width_2 = lorawan:addr_bit_len(DevAddr02),
+    Width_2 = lora_subnet:addr_bit_len(DevAddr02),
     ?assertEqual(17, Width_2),
 
-    Width0 = lorawan:addr_bit_len(DevAddrLegacy),
+    Width0 = lora_subnet:addr_bit_len(DevAddrLegacy),
     ?assertEqual(24, Width0),
-    Width1 = lorawan:addr_bit_len(DevAddr1),
+    Width1 = lora_subnet:addr_bit_len(DevAddr1),
     ?assertEqual(10, Width1),
-    Width2 = lorawan:addr_bit_len(DevAddr2),
+    Width2 = lora_subnet:addr_bit_len(DevAddr2),
     ?assertEqual(17, Width2),
 
-    NwkAddr0 = lorawan:nwk_addr(DevAddr00),
+    NwkAddr0 = lora_subnet:nwk_addr(DevAddr00),
     ?assertEqual(0, NwkAddr0),
-    NwkAddr1 = lorawan:nwk_addr(DevAddr01),
+    NwkAddr1 = lora_subnet:nwk_addr(DevAddr01),
     ?assertEqual(16, NwkAddr1),
-    NwkAddr2 = lorawan:nwk_addr(DevAddr02),
+    NwkAddr2 = lora_subnet:nwk_addr(DevAddr02),
     ?assertEqual(8, NwkAddr2),
 
     %% Backwards DevAddr compatibility test
@@ -267,36 +267,36 @@ netid_test() ->
     %% but if we compute the associated DevAddr for this subnet (for the Join request)
     %% we'll get a new DevAddr associated with a current and proper NetID.
     %% In other words, DevAddr00 is not equal to DevAddr000.
-    Subnet0 = lorawan:subnet_from_devaddr(DevAddr00, NetIDList),
+    Subnet0 = lora_subnet:subnet_from_devaddr(DevAddr00, NetIDList),
     io:format("Subnet0 ~8.16.0B~n", [Subnet0]),
     ?assertEqual(0, Subnet0),
-    DevAddr000 = lorawan:devaddr_from_subnet(Subnet0, NetIDList),
+    DevAddr000 = lora_subnet:devaddr_from_subnet(Subnet0, NetIDList),
     io:format("DevAddr00 ~8.16.0B~n", [DevAddr00]),
     io:format("DevAddr000 ~8.16.0B~n", [DevAddr000]),
     %% By design the reverse DevAddr will have a correct NetID
     ?assertNotEqual(DevAddr000, DevAddr00),
     ?assertEqual(16#FE000080, DevAddr000),
-    {ok, DevAddr000NetID} = lorawan:parse_netid(DevAddr000),
+    {ok, DevAddr000NetID} = lora_subnet:parse_netid(DevAddr000),
     ?assertEqual(NetID00, DevAddr000NetID),
-    NwkAddr000 = lorawan:nwk_addr(DevAddr000),
+    NwkAddr000 = lora_subnet:nwk_addr(DevAddr000),
     ?assertEqual(NwkAddr0, NwkAddr000),
 
-    Subnet1 = lorawan:subnet_from_devaddr(DevAddr01, NetIDList),
+    Subnet1 = lora_subnet:subnet_from_devaddr(DevAddr01, NetIDList),
     io:format("Subnet1 ~8.16.0B~n", [Subnet1]),
     ?assertEqual((1 bsl 7) + 16, Subnet1),
-    DevAddr001 = lorawan:devaddr_from_subnet(Subnet1, NetIDList),
+    DevAddr001 = lora_subnet:devaddr_from_subnet(Subnet1, NetIDList),
     io:format("DevAddr01 ~8.16.0B~n", [DevAddr01]),
     io:format("DevAddr001 ~8.16.0B~n", [DevAddr001]),
     ?assertEqual(DevAddr001, DevAddr01),
 
-    Subnet1 = lorawan:subnet_from_devaddr(DevAddr01, NetIDList),
+    Subnet1 = lora_subnet:subnet_from_devaddr(DevAddr01, NetIDList),
     ?assertEqual((1 bsl 7) + 16, Subnet1),
-    DevAddr001 = lorawan:devaddr_from_subnet(Subnet1, NetIDList),
+    DevAddr001 = lora_subnet:devaddr_from_subnet(Subnet1, NetIDList),
     ?assertEqual(DevAddr001, DevAddr01),
 
-    Subnet2 = lorawan:subnet_from_devaddr(DevAddr02, NetIDList),
+    Subnet2 = lora_subnet:subnet_from_devaddr(DevAddr02, NetIDList),
     ?assertEqual((1 bsl 7) + (1 bsl 10) + 8, Subnet2),
-    DevAddr002 = lorawan:devaddr_from_subnet(Subnet2, NetIDList),
+    DevAddr002 = lora_subnet:devaddr_from_subnet(Subnet2, NetIDList),
     ?assertEqual(DevAddr002, DevAddr02),
 
     ok.
