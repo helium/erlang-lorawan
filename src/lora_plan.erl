2a0d424eba98f927880adb222444b52f91db127d
Michael Vierling <michael@helium.foundation>
2022-11-29 15:38:19 -0800

    update base_region strings

--- src/lora_plan.erl
+++ src/lora_plan.erl
@@ -739,7 +739,7 @@ round_frequency(Value, Precision) ->
 plan_eu868_A() ->
     Plan = #channel_plan{
         channel_plan_id = 1,
-        plan_name = 'EU868_A',
+        plan_name = 'EU868',
         base_region = 'EU868',
         dynamic_plan = true,
         float_precision = 1,
@@ -880,7 +880,7 @@ plan_au915_SB2() ->
     Plan = #channel_plan{
         channel_plan_id = 5,
         plan_name = 'AU915_SB2',
-        base_region = 'AU915',
+        base_region = 'AU915_SB2',
         dynamic_plan = false,
         float_precision = 1,
         min_freq = 915.0,
@@ -932,7 +932,7 @@ plan_au915_DP() ->
     Plan = #channel_plan{
         channel_plan_id = 5,
         plan_name = 'AU915_DP',
-        base_region = 'AU915',
+        base_region = 'AU915_DP',
         dynamic_plan = false,
         float_precision = 1,
         min_freq = 915.0,
@@ -983,7 +983,7 @@ plan_au915_SB5() ->
     Plan = #channel_plan{
         channel_plan_id = 5,
         plan_name = 'AU915_SB5',
-        base_region = 'AU915',
+        base_region = 'AU915_SB5',
         dynamic_plan = false,
         float_precision = 1,
         min_freq = 915.0,
@@ -1170,7 +1170,7 @@ plan_as923_1B() ->
     Plan = #channel_plan{
         channel_plan_id = 7,
         plan_name = 'AS923_1B',
-        base_region = 'AS923_1',
+        base_region = 'AS923_1B',
         dynamic_plan = true,
         float_precision = 1,
         min_freq = 919.0,
